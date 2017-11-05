bc5e259586f3af958312d5e9e0efa591f1c755a7
Paul Haddad <paulh16@gmail.com>
2017-11-05 17:39:35 -0600

    Do es6 destructuring exercises

--- src/es6.js
+++ src/es6.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 
 // Refactor the following code to use the specified ES6 features.
 // There are no automated tests.
@@ -7,50 +6,47 @@
 //----------------
 // const, =>, default parameters, arrow functions default return statements using ()
 
-var food = 'pineapple';
+const foodSample = 'pineapple';
 
-var isMyFavoriteFood = function(food) {
-  food = food || 'thousand-year-old egg'; //This sets a default value if `food` is falsey
-  return food === 'thousand-year-old egg';
-};
+const isMyFavoriteFood = (food = 'thousand-year-old egg') => food === 'thousand-year-old egg';
 
-var isThisMyFavorite = isMyFavoriteFood(food);
+const isThisMyFavorite = isMyFavoriteFood(foodSample);
+
+console.log(isThisMyFavorite);
 
 //----------------
 //const, class, template literals, enhanced object literals (foo: foo, -> foo,)
 
-var User = function(options) {
+function User(options) {
   this.username = options.username;
   this.password = options.password;
-  this.sayHi = function() {
-    return this.username + ' says hello!';
-  };
+  this.sayHi = () => `${this.username} says hello!`;
 }
 
-var username = 'JavaScriptForever';
-var password = 'password';
+const username = 'JavaScriptForever';
+const password = 'password';
 
-var me = new User({
-  username: username,
-  password: password,
+const me = new User({
+  username,
+  password,
 });
+
+console.log(me);
+console.log(me.sayHi());
 
 // ----------------
 // let, const, =>, ... (spread operator)
 
-var addArgs = function () {
-  var sum = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
-  return sum;
+const addArgs = function (...numbers) {
+  return numbers.reduce((sum, num) => sum + num);
 };
 
-var argsToCb = function (cb) {
-  var args = Array.prototype.slice.call(arguments);
-  return cb.apply(null, args.splice(1));
+const argsToCb = function (cb, ...numbers) {
+  return cb(...numbers);
 };
 
-var result = argsToCb(addArgs, 1, 2, 3, 4, 5); //result should be 15
+const result = argsToCb(addArgs, 1, 2, 3, 4, 5); //result should be 15
+
+console.log(result);
 
 /* eslint-enable */
